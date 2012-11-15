5f399847126c2c50967c4a66c54fd60b0498838a
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-11-15 13:06:07 +0100

    Mas mejoras

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -18,7 +18,6 @@ Spree.config do |config|
   config.currency = "EUR"
   config.display_currency = false
   config.products_per_page = 12
-  config.minimun_price_to_show = 30
   config.default_meta_description = "Neumaticos para turismos, motos, furgonetas, camiones y agricolas a precios muy bajos. Le ofrecemos los neumaticos mas baratos. Precios inmejorables, sin gastos de envio para la  peninsula, servicio economico de montaje en casi toda la Peninsula, pedidos faciles de efectuar, sistema flexible de pago, gran variedad, en 48 horas los tendra en su domicilio"
   config.default_meta_keywords = "Neumaticos, ruedas, coche, furgoneta, caravana, camion, agricolas, moto, ofertas, baratos, todas, marcas, todo, tiempo"
 end
